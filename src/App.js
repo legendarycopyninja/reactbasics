be8cf400c5b2695f5b751dee315ae510542aa335
Dilli_Rao <101472151+legendarycopyninja@users.noreply.github.com>
2023-01-27 23:29:25 +0530

    commit one

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import logo from './logo.svg';
+import './App.css';
+import MainContent from './MainContent';
+
+function Header(){
+  return (
+    <header>
+    <nav>
+    <img src={logo} className="App-logo" alt="logo" />
+    <h2>ReactFacts</h2>
+    <ol className='nav-items' >
+      <li>Pricing</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ol>
+    </nav>
+    </header>
+  )
+}
+
+
+
+function Footer() {
+<footer>© 2023 Dilli development. All rights reserved.</footer>
+
+}
+
+function App() {
+  return (
+    <div className="App-header">
+      <Header />
+      <MainContent />
+      <Footer />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/MainContent.js
@@ -0,0 +1,14 @@
+export default function MainContent() {
+    return (
+    
+            <div className="content-header">
+             <h1>Fun Facts About React</h1>
+              <ul>
+                <li>React is Not A Framework</li>
+                <li>React development And SEO Go Together</li>
+                <li>Virtual DOM is Faster Than Real DOM</li>
+                <li>React Has a Strong Community</li>
+              </ul>
+              </div>
+    )
+    }
